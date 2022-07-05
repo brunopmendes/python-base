186d22b0792a709e4f17e9ba7a6c40b4355158ab
brunopmendes <brupmendes@icloud.com>
2022-07-05 20:51:43 -0300

    ♻️ refactor: incluindo try, exceptions

---
+++ day3_IO_algoritmos_condicionais_repeticoes/Exceptions/hello.py
@@ -0,0 +1,71 @@
+"""
+Hello World Multi Linguas.
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+Como usar:
+Tenha a variavel LANG devidamente configurada:
+    export LANG=pt_BR
+Ou informe atraves do CLI argument `--lang`
+Ou o usuario tera que digitar.
+Execucao:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.1.3"
+__author__ = "Bruno Mendes"
+__license__ = "Unlicense"
+
+from email import message
+import os
+import sys
+
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+for arg in sys.argv[1:]:
+    try:
+        key, value = arg.split("=")
+    except ValueError as e:
+        # TODO: Logging
+        print(f"[ERROR] {str(e)}")
+        print("You need to use `=`")
+        print(f"You passed  {arg}")
+        print("Try with --key=value")
+        sys.exit(1)
+
+    key = key.lstrip("-").strip()
+    value = value.strip()
+
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repeticao
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Ola, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour Monde",
+}
+
+try:
+    message = msg[current_language]
+except KeyError as e:
+    print(f"[ERROR] {str(e)}")
+    print(f"Language is invalid, choose from: {list(msg.keys())}")
+    sys.exit(1)
+
+print(message * int(arguments["count"]))
---
+++ day3_IO_algoritmos_condicionais_repeticoes/Exceptions/infixcalc.py
@@ -0,0 +1,69 @@
+"""Calculadora infix.
+Funcionamento:
+[operacao] [n1] [n2]
+Operacoes:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+Uso:
+$ infixcalc.py sum 5 2
+7
+$ infixcalc.py mul 10 5
+50
+$ infixcalc.py 
+operacao: sum
+n1: 5
+n2: 4
+9
+"""
+__version__ = "0.1.0"
+
+import sys
+
+arguments = sys.argv[1:]
+
+if not arguments:
+    operation = input("operacao:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Numero de argumentos invalidos")
+    print("ex: `sum 5 5`")
+    sys.exit(1)
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+if operation not in valid_operations:
+    print("Operacao invalida")
+    print(valid_operations)
+    sys.exit(1)
+
+validated_nums = []
+for num in nums:
+    # TODO: Repeticao while + exceptions
+    if not num.isdigit():
+        print(f"Numero invalido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+try:
+    n1, n2 = validated_nums
+except ValueError as e:
+    print(str(e))
+    sys.exit(1)
+
+# TODO: Usar dict de funcoes
+if operation == "sum":
+    print(n1 + n2)
+elif operation == "sub":
+    print(n1 - n2)
+elif operation == "mul":
+    print(n1 * n2)
+elif operation == "div":
+    print(n1 / n2)
