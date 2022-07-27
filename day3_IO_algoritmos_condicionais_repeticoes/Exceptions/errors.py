4b6f01588a5e6cd493906a070a7555c61610d2ce
brunopmendes <brupmendes@icloud.com>
2022-07-26 22:07:44 -0300

    ♻️ refactor(errors): lopping com para abrir arquivo

--- day3_IO_algoritmos_condicionais_repeticoes/Exceptions/errors.py
+++ day3_IO_algoritmos_condicionais_repeticoes/Exceptions/errors.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentError
 import os
+import re
 import sys
 import logging
 import time
@@ -11,16 +13,21 @@ log = logging.Logger("errors")
 
 def try_to_open_file(file_path, retry=1):
     """Tries to open a file, if error, retries n times"""
-    for attempt in range(1, retry + 1):
-        try:
-            return open(file_path).readlines() # FileNotFoundError
-        except FileNotFoundError as e:
-            log.error("ERRO: %s", e)
-            time.sleep(1)
-        else:
-            print("Sucesso!")
-        finally:
-            print("Execute isso sempre!")
+    if retry > 999:
+        raise ValueError("Retry cannot be above 999")
+
+    try:
+        return open(file_path).readlines() # FileNotFoundError
+    except FileNotFoundError as e:
+        log.error("ERRO: %s", e)
+        time.sleep(2)
+        if retry > 1:
+            # recursao
+            try_to_open_file(file_path, retry=retry-1)
+    else:
+        print("Sucesso!")
+    finally:
+        print("Execute isso sempre!")
     return []
 
 for line in try_to_open_file("names.txt", 5):
