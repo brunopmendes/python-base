795bfb1d863d46ec0652cccf1a621eecb56c6a6f
brunopmendes <brupmendes@icloud.com>
2022-07-26 22:00:11 -0300

    feat(erros): retry com for

--- day3_IO_algoritmos_condicionais_repeticoes/Exceptions/errors.py
+++ day3_IO_algoritmos_condicionais_repeticoes/Exceptions/errors.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 
 import os
-import sys 
+import sys
+import logging
+import time
+
+log = logging.Logger("errors")
 
 # EAFP - Easy to Ask Forgiveness than permission
 
-try:
-    names = open("names.txt").readlines() # FileNotFoundError
-except FileNotFoundError as e:
-    print(f"{e}")
-    sys.exit(1)
-    # TODO: Usar retry
-else:
-    print("Sucesso!")
-finally:
-    print("Execute isso sempre!")
+def try_to_open_file(file_path, retry=1):
+    """Tries to open a file, if error, retries n times"""
+    for attempt in range(1, retry + 1):
+        try:
+            return open(file_path).readlines() # FileNotFoundError
+        except FileNotFoundError as e:
+            log.error("ERRO: %s", e)
+            time.sleep(1)
+        else:
+            print("Sucesso!")
+        finally:
+            print("Execute isso sempre!")
+    return []
 
-try:
-    print(names[2])
-except:
-    print("Missing name in the list")
-    sys.exit(1)
+for line in try_to_open_file("names.txt", 5):
+    print(line)
